d9c9cb59d453d4cdabce9b34e902e9186bc94f39
Olwethu Ntsukumbini <olwethu.ntsukumbini@umuzi.org>
2024-07-17 14:32:35 +0200

    added inline descriptions and picture descriptions

--- between_two_sets.py
+++ between_two_sets.py
@@ -1,3 +1,20 @@
+"""
+There will be two arrays of integers. Determine all integers that satisfy the following two conditions:
+
+The elements of the first array are all factors of the integer being considered
+The integer being considered is a factor of all elements of the second array
+These numbers are referred to as being between the two arrays. Determine how many such numbers exist.
+
+Example
+a = [2,6]
+b = [24,36]
+
+There are two numbers between the arrays: 6 and 12.
+6 % 2 = 0, 6 % 6 = 0, 24%6 = 0 and 36 % 6 = 0 for the first value.
+12 % 2 = 0, 12 % 6 = 0 and 24 % 12 = 0, 36 % 12 = 0  for the second value. Return 2.
+"""
+
+
 def get_total():
     # create a variable to store the total
     # create a variable to store current factorial
--- company_logo.py
+++ company_logo.py
@@ -1,3 +1,17 @@
+"""
+A newly opened multinational brand has decided to base their company logo on the three most common characters in the company name. 
+They are now trying out various combinations of company names and logos based on this condition. Given a string (s), 
+which is the company name in lowercase letters, your task is to find the top three most common characters in the string.
+
+- Print the three most common characters along with their occurrence count.
+- Sort in descending order of occurrence count.
+- If the occurrence count is the same, sort the characters in alphabetical order.
+For example, according to the conditions described above,
+
+GOOGLE would have it's logo with the letters G,O,E.
+"""
+
+
 def company_logo(s):
     # Print 3 common characters and the number of characters
     # Sort in decending order of the number of characters
